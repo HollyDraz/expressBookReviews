c08d509b68a9bd7f7e0a062c548a7bc9aa2277c6
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-04-08 22:32:14 -0500

    added get by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -87,10 +87,43 @@ public_users.get('/author/:author', (req, res) => {
 
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Fetch books by title using Promise
+const fetchBooksByTitle = (title) => {
+  return new Promise((resolve, reject) => {
+      const bookKeys = Object.keys(books);
+      const matchingBooks = bookKeys
+          .map(key => books[key])
+          .filter(book => book.title.toLowerCase() === title.toLowerCase());
+      
+      if (matchingBooks.length > 0) {
+          resolve(matchingBooks);
+      } else {
+          reject(new Error("No books found for the given title"));
+      }
+  });
+};
+
+// Get all books based on title
+public_users.get('/title/:title', (req, res) => {
+  const title = req.params.title;
+  fetchBooksByTitle(title)
+      .then(matchingBooks => {
+          const formattedBooks = JSON.stringify(matchingBooks, null, 2);
+          res.setHeader('Content-Type', 'application/json');
+          res.status(200).send(formattedBooks);
+      })
+      .catch(error => {
+          if (error.message === "No books found for the given title") {
+              res.status(404).json({ message: error.message });
+          } else {
+              res.status(500).json({
+                  message: "Error retrieving books by title",
+                  error: error.message
+              });
+          }
+      });
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
